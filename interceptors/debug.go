ab25672e816a5d7a16460c08a94ef403bd795b65
Ben Pate <ben@pate.org>
2015-07-03 11:31:33 -0600

    Debug Interceptor

--- interceptors/debug.go
+++ interceptors/debug.go
@@ -4,25 +4,46 @@ import (
 	"fmt"
 	"github.com/brandonromano/wrecker"
 	"net/http"
+	"strconv"
 )
 
 // Debug is a sample Interceptor that adds debugging output to every request.
-func Debug(auth string) wrecker.Interceptor {
+func Debug() wrecker.Interceptor {
 
 	return wrecker.Interceptor{
 
 		// This is executed on every Request before its sent to the server
-		WreckerRequest: func(r *wrecker.Request) error {
+		Request: func(r *wrecker.Request) error {
 
 			return nil
 		},
 
 		RawRequest: func(r *http.Request) error {
 
+			fmt.Println("")
+			fmt.Println("HTTP Request")
+			fmt.Println("-------------")
+			fmt.Println("URL: ", r.URL.String())
+			fmt.Println("Method: ", r.Method)
 			return nil
 		},
 
-		RawResponse: func(r *http.Response) error {
+		RawResponse: func(r *http.Response, body []byte) error {
+
+			fmt.Println("")
+			fmt.Println("HTTP Response")
+			fmt.Println("-------------")
+
+			fmt.Println("Status Code: ", strconv.Itoa(r.StatusCode))
+			fmt.Println("Status: ", r.Status)
+			fmt.Println("Headers:")
+
+			for i := range r.Header {
+				fmt.Println("- ", i, ": ", r.Header.Get(i))
+			}
+
+			fmt.Println("Body: ", string(body))
+			fmt.Println("")
 
 			return nil
 		},
