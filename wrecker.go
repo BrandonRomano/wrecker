83ebfa18bb44493e2a1edf9ab4f5d9dd32888cff
Ben Pate <ben@pate.org>
2015-07-08 17:16:36 -0600

    Relocate Interceptor definition
    Moving type definition into the main wrecker.go file to test if this
    helps the Travis tests to pass.

--- wrecker.go
+++ wrecker.go
@@ -18,6 +18,14 @@ type Wrecker struct {
 	Interceptors       []Interceptor
 }
 
+// Interceptor contains functions that receive (and can modify) a
+// wrecker.Request before it is sent to the server.  The Wrecker instance
+// maintains an array of Interceptors that are applied to every
+// wrecker.Request in the order that they were assigned.
+type Interceptor struct {
+	WreckerRequest func(*Request) error
+}
+
 func New(baseUrl string) *Wrecker {
 	return &Wrecker{
 		BaseURL: baseUrl,
