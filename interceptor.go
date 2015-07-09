89d4eecad12828c015b3820ac95e6dabc4ec7f0a
Ben Pate <ben@pate.org>
2015-07-08 18:00:40 -0600

    testing... should work now.

--- interceptor.go
+++ interceptor.go
@@ -1,9 +1,5 @@
 package wrecker
 
-import (
-	"net/http"
-)
-
 // Interceptor contains functions that receive (and can modify) a
 // wrecker.Request before it is sent to the server.  The Wrecker instance
 // maintains an array of Interceptors that are applied to every
